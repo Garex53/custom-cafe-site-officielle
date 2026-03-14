99771acce46a3c188057ce1963533549697b886d
Garex53 <158852477+Garex53@users.noreply.github.com>
2026-03-14 13:40:34 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,28 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: "REPLACE_API_KEY",
-  authDomain: "REPLACE_PROJECT.firebaseapp.com",
-  projectId: "REPLACE_PROJECT_ID",
-  storageBucket: "REPLACE_PROJECT.firebasestorage.app",
-  messagingSenderId: "REPLACE_SENDER_ID",
-  appId: "REPLACE_APP_ID",
-  measurementId: "REPLACE_MEASUREMENT_ID"
+  apiKey: "AIzaSyAkzbbSR0ApyDD3rT6yNo0EggG7nbZmeYE",
+  authDomain: "custom-cafe-site-officielle.firebaseapp.com",
+  projectId: "custom-cafe-site-officielle",
+  storageBucket: "custom-cafe-site-officielle.firebasestorage.app",
+  messagingSenderId: "456151591360",
+  appId: "1:456151591360:web:ba09a241b2a8897df84025",
+  measurementId: "G-W19VHPXMYV"
 };
 
-let app = null;
-let firebaseReady = false;
-let firebaseConfigError = "";
+const app = initializeApp(firebaseConfig);
 
-try {
-  if (!firebaseConfig.apiKey || firebaseConfig.apiKey.startsWith("REPLACE_")) {
-    throw new Error("firebase-config.js n'est pas encore rempli");
-  }
-  app = initializeApp(firebaseConfig);
-  firebaseReady = true;
-} catch (error) {
-  firebaseConfigError = error?.message || "Configuration Firebase invalide";
-  console.warn("Firebase désactivé:", firebaseConfigError);
-}
-
-export { app, firebaseReady, firebaseConfigError, firebaseConfig };
+export { app };
